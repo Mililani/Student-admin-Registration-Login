2224dd03c6cc80d603aed368dab97502e2e06da0
Mililani <mmrakhetsi00@gmail.com>
2022-09-05 21:30:24 +0200

    first init

---
+++ src/main/java/com/studentadmin/studentadmin/StudentadminApplication.java
@@ -0,0 +1,13 @@
+package com.studentadmin.studentadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentadmin/studentadmin/StudentadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentadmin.studentadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
